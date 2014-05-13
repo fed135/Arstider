1cd58dbba2b58c7507a3615eec390453e40ebe91
fed135 <fredericcharette@gmail.com>
2014-05-13 12:10:08 -0400

    final image optimizer

--- tools/imageOptimizer.py
+++ tools/imageOptimizer.py
@@ -1,30 +1,43 @@
 import os, sys
+from PIL import Image
 
-def png_optimize(dir):
-    findcmd = 'find %s -name "*.png" -print' % dir
+totalsaved = 0
+
+def optimize_images(dir):
 
     count = 0
-    for f in os.popen(findcmd).readlines():
-        count += 1
-        file = str(f[:-1])
-        image = Image.open(file)
-        image.save(file,quality=85,optimize=True)
-    print "Optimized %d .png files" % count
+    for root, dirs, files in os.walk(dir):
+        path = root.split("/")
+        print (len(path) - 1) *'---' , os.path.basename(root)       
+        for file in files:
+            if ".png" in file:
+                resave_image(root +"/"+ file)
+                count += 1
+            if ".jpg" in file:
+                resave_image(root +"/"+ file)
+                count += 1
+    print " "
+    print "Done!"
+    print "Optimized %d image files" % count
     
-def jpg_optimize(dir):
-    findcmd = 'find %s -name "*.jpg" -print' % dir
 
-    count = 0
-    for f in os.popen(findcmd).readlines():
-        count += 1
-        file = str(f[:-1])
-        image = Image.open(file)
-        image.save(file,quality=70,optimize=True)
-    print "Optimized %d .jpg files" % count
+def resave_image(file):
+    global totalsaved
+    print " "
+    print "optimizing " + file + "..."
+    prevsize = os.path.getsize(file)
+    image = Image.open(file)
+    image.save(file,quality=85,optimize=True)
+    print str(prevsize) + " -----> " + str(os.path.getsize(file))
+    saved = prevsize - os.path.getsize(file)
+    totalsaved += saved
+    if((prevsize - os.path.getsize(file)) < 0):
+        print " Failed! File gained " +str(saved) + " bytes"
+    else:
+        print " Success! File lost " + str(saved) + " bytes (" + str(round((int(os.path.getsize(file))/int(prevsize))*100, 2)) + "%)"
+
 
 if __name__ == "__main__":
-    print "Compressing PNG images..."
-    png_optimize(sys.argv[1])
-    print "Compressing JPG images..."
-    jpg_optimize(sys.argv[1])
-    print "Done!"
+    print "Compressing images..."
+    optimize_images(sys.argv[1])
+    print "Saved a total of " + str(totalsaved) + " bytes"
