8f67466b7392ce890046efc0d03f019a4bd29b6c
fed135 <fredericcharette@gmail.com>
2014-06-10 15:20:03 -0400

    screen state save

--- src/sdk/Screen.js
+++ src/sdk/Screen.js
@@ -175,6 +175,17 @@ define("Arstider/Screen", [
 	};
 	
 	/**
+	 * Saves the state of a screen so you can hold, show another screen, then load back the state
+	 * @type {function(this:Screen)}
+	 * @param {string} name The name to store the state under
+	 * @param {boolean|null} save Whether to save thatr state in localStorage
+	 */
+	Screen.prototype.saveStateAs = function(name, save){
+		this.__savedState = true;
+		Arstider.savedStates[name] = this;
+	};
+
+	/**
 	 * Resizes the screen when globalScale changes
 	 * @private
 	 * @type {function(this:Screen)}
