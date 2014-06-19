c354cd0fa266bf990ec2a29a31474489f701f6c2
FredoFidel <fredo@meetfidel.com>
2014-06-19 13:37:40 -0400

    Add deep clone features

--- src/sdk/core/Utils.js
+++ src/sdk/core/Utils.js
@@ -373,6 +373,45 @@ Arstider.clone = function(obj, includeMethods, prefix){
 };
 
 /**
+ * Utility function to clone an object recursively
+ * @memberof Arstider
+ * @const
+ * @param {Object} obj The object to copy
+ */
+Arstider.deepClone = function(obj) {
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = Arstider.deepClone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = Arstider.deepClone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
+/**
  * Supers the values of a module to it's parent module
  * @memberof Arstider
  * @param {*} child The child that will super to a defined inherited parent - requires the constructor to have been Inherited at least once
