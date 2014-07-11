7a474edb0947d93682ee51e2dc44a2ce82a14e44
FredoFidel <fredo@meetfidel.com>
2014-07-11 11:40:59 -0400

    Remove log

--- src/sdk/Fonts.js
+++ src/sdk/Fonts.js
@@ -206,7 +206,6 @@
 				cache:false,
 				callback:function(file){
 					var fontList = file;
-					console.log(fontList)
 					for(var i in fontList){
 						fontList[i].name = i;
 						this.create(fontList[i]);
